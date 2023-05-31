aab3890acc010e5c3165812da2104dda7aa2f1f5
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-31 08:35:47 -0500

    After utilizing the third party body-parser package, after logging req.body, we see our data logged inside of an an object, this approach streamlines the parsing process, so we don't need to write all that logic ourselves

--- 05_Express.js/03_ExpressPractice/parseData.js
+++ 05_Express.js/03_ExpressPractice/parseData.js
@@ -9,10 +9,39 @@ const express = require("express");
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    // console.log("Always runs !");
-    next();
-})
+// app.use('/', (req, res, next) => {
+//     // console.log("Always runs !");
+//     next();
+// })
+
+
+// Below is our parser, to parse the inputted 
+// req.body, but we need a third party package
+// called body-parser, so enter 'npm install --save body-parser'
+
+const bodyParse = require("body-parser");
+
+// and now that we've got it in our package.json, 
+// let's use it 
+
+// app.use(bodyParse.urlencoded())
+
+// Also note, apparently we need to add: 
+
+app.use(bodyParse.urlencoded({extended: false}));
+// if we omit the extended: false, we see: 
+
+// Wed, 31 May 2023 13:28:40 GMT body-parser 
+// deprecated undefined extended: 
+// provide extended option at parseData.js:27:19
+
+// So we call app.use, then bodyParse.urlencoded(), 
+// important to note we need to execute with () b/c 
+// it registers a middlewar, important side-note. 
+// this method works for us, but it wouldn't work if 
+// we needed to parse a jsonfile or other file type, 
+// it works well for us in this instance with our 
+// text from the submit form
 
 app.use('/add-product', (req, res, next) => {
     // console.log('in another midware');
@@ -40,6 +69,20 @@ app.use('/product', (req, res, next) => {
 
     console.log(req.body);
     // What happens if we look at request body? 
+    // Well we see undefined is logged, why is that?
+    // it's because express doesn't auto-parse, we need 
+    // to register a parser
+
+    // And yes, now in the console, we see the req.body 
+    // was now parsed with our body-parser package, 
+    // { title: 'Summa Theologica' }
+    // is logged to the console 
+
+    // Which is much simpler than the old approach 
+    // we took in the first few lectures where we 
+    // did all of this work manually, this is the 
+    // benefit express.js gives us 
+
    res.redirect('/')
 
     // as a note: we can interchange app.use('/product)
