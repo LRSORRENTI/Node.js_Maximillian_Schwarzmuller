f31ae030be2847141887fda1d793af970b8b1a1d
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-05 10:23:13 -0500

    Added start script to package json

--- 05_Express.js/04_Express_Router/app.js
+++ 05_Express.js/04_Express_Router/app.js
@@ -1,1 +1,21 @@
 // add stuff later 
+
+const app = express();
+
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop")
+
+// so our adminRoutes is importing the routes 
+// we have in our admin js file
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes)
+// just as before, the order matters, if we put 
+// the above line below the app.use('/')
+// we would never reach it
+
+app.use(shopRoutes)
+
+app.listen(3000)
