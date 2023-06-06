583ba620ad57e9d7e70c0d4f509196ec8c6b3c3d
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-06 07:45:03 -0500

    Added app.use right before calling app.listen, also can use chaining methods like 'app.use((req,res,next)=>{res.status(404).send('<html')})

--- 05_Express.js/04_Express_Router/app.js
+++ 05_Express.js/04_Express_Router/app.js
@@ -22,5 +22,28 @@ app.use(adminRoutes)
 // we would never reach it
 
 app.use(shopRoutes)
+// Also note if we were to change the order 
+// of these app.use(adminRoutes), the adminRoutes 
+// would never be reached, the order in which we pass 
+// in the routes matters
+
+/*
+
+=====================
+ADDING 404 ERROR PAGE
+=====================
+
+To include an error page for our shop, 
+at the bottom we add a catch-all middleware
+
+
+
+*/
+app.use((req, res, next) => {
+     res.status(404).send('<h1>ERROR:404 Page Not Found</h1>')
+     
+})
+// WE can chain these method calls together in 
+// the res if we want
 
 app.listen(3000)
