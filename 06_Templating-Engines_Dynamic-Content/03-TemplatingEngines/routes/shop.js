b2dd694b33ec1a813a0d4b56010d7cb7e5bff05a
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-08 10:38:51 -0500

    Added section on dynamically rendering content in pug, the syntax is unfamiliar but looks cool

--- 06_Templating-Engines_Dynamic-Content/03-TemplatingEngines/routes/shop.js
+++ 06_Templating-Engines_Dynamic-Content/03-TemplatingEngines/routes/shop.js
@@ -11,7 +11,15 @@ router.get('/', (req, res, next) => {
   // console.log('shop.js', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   // now we need to change to our pug html
-  res.render('shop');
+  const products = adminData.products;
+  // we need to extract the data houseed in
+  // .products and render it dynamically, 
+  // to achieve this we pass in a second 
+  // argument below
+  // res.render('shop');
+  res.render('shop', {prods: products, docTitle: 'Pug Shoppe'});
+  // now back in our pug file, shop.pug we can 
+  // access prods 
   // render is a special method which will use the 
   // default templating engine 
 });
