ac8f42ed74723910a21f836e7c078562f36610da
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-09 08:23:47 -0500

    finished section on handlebars, next is ejs

--- 06_Templating-Engines_Dynamic-Content/03-TemplatingEngines/app.js
+++ 06_Templating-Engines_Dynamic-Content/03-TemplatingEngines/app.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHBS = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug')
+
+app.engine('handlebars', expressHBS());
+// now when we use handlebars, we need to import it 
+// above with the const expressHBS = require function,
+// but we also need to use the app.engine and pass in 
+// two arguments, 'handlebars', and expressHBS, but we 
+// need to invoke it to initialize the engine expressHBS()
+app.set('view engine', 'handlebars')
 // we also should explicitly name where our views folder 
 // is: 
 app.set('views', 'views')
@@ -24,7 +32,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: '404 Page Not Found'})
 });
 
 app.listen(3000);
