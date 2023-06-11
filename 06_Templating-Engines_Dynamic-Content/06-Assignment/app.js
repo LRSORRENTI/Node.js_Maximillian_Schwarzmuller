867a579dddb6c506ad5e00ccd596a57ca3917c75
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-11 09:13:28 -0500

    Added routes in app.js

--- 06_Templating-Engines_Dynamic-Content/06-Assignment/app.js
+++ 06_Templating-Engines_Dynamic-Content/06-Assignment/app.js
@@ -1,4 +1,20 @@
 const express = require("express")
+
 const bodyParser = require("body-parser")
+
 const app = express()
+
+app.get('/', (req, res, next) => {
+    res.render()
+})
+
+app.get('/users', (req, res, next) => {
+    res.render();
+})
+
+app.post('/add-user', (req, res, next) => {
+   res.redirect('/users')
+})
+
+
 app.listen(3000)
