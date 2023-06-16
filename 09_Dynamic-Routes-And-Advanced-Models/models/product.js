6de31779e2e5fda551682b45f563801d40dc7cb1
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-16 09:09:00 -0500

    added a way to generate a unique id for an instance of product using this.id = Math.random().toString(), which isn't the ideal way of creating unique ID's but for the purpose of learning the concept, it will suffice, inside product.js in the save() method in the Product class

--- 09_Dynamic-Routes-And-Advanced-Models/models/product.js
+++ 09_Dynamic-Routes-And-Advanced-Models/models/product.js
@@ -25,7 +25,28 @@ module.exports = class Product {
     this.price = price;
   }
 
+// Inside of here we need to implement a way of 
+// ensuring every products has a unique id, that 
+// way we can implement a description for a 
+// given product 
+
   save() {
+    // We can do this inside of save
+    // we can add this.id, which will add 
+    // a new id prop to the product object
+    // we currently have 
+
+    // The ideal way is not math.random but for 
+    // the purposes of learning it will suffice, 
+    // there are better third party packages to 
+    // create unique ID's but this will work for 
+    // us 
+    this.id = Math.random().toString();
+    // after the above runs, we have a unique ID 
+    // saved to the current instance of product 
+    // using the 'this' keyword, and we generate 
+    // a number with math.random, then convert 
+    // it to a string with .toString()
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
