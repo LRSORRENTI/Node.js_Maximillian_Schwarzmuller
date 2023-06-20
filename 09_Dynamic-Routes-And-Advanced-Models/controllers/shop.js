c00dadf9745cd28055568126cef094026a0ed21d
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-20 08:23:44 -0500

    added var for Cart in shop.js controller

--- 09_Dynamic-Routes-And-Advanced-Models/controllers/shop.js
+++ 09_Dynamic-Routes-And-Advanced-Models/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -10,22 +11,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-// Here is where we add the dynamic route controller
-
 exports.getProduct = (req, res, next) => {
-    const prodId = req.params.productId;
-    Product.findById(prodId, myProduct =>{
-      console.log(product)
-      res.render('shop/product-detail.ejs', 
-      {product: myProduct, 
-        pageTitle: myProduct.title,
-      path: '/products' })
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
     });
-    // remember inside of routes, productId 
-    // is the name we gave the router.get('/products/productId')
-    //  res.redirect('/');
-  };
-
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -42,6 +37,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
