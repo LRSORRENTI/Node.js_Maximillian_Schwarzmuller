f1d1efcabd8935dd7f189eeef46552abf3771d61
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-28 08:31:17 -0500

    Added back in some of the logic for admin edit and delete product functionality

--- 12-NoSQL-MongoDB/controllers/admin.js
+++ 12-NoSQL-MongoDB/controllers/admin.js
@@ -29,41 +29,47 @@ exports.postAddProduct = (req, res, next) => {
   // a new row with a new book id, title, price, description
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   Product.findById(prodId, product => {
-//     if (!product) {
-//       return res.redirect('/');
-//     }
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit Product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product
-//     });
-//   });
-// };
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+  .then(product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+    .catch(err => console.log(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   const updatedTitle = req.body.title;
-//   const updatedPrice = req.body.price;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDesc = req.body.description;
-//   const updatedProduct = new Product(
-//     prodId,
-//     updatedTitle,
-//     updatedImageUrl,
-//     updatedDesc,
-//     updatedPrice
-//   );
-//   updatedProduct.save();
-//   res.redirect('/admin/products');
-// };
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log('Updated Product?');
+      res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err))
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
