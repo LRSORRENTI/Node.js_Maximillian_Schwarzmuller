c0d9fdedff2c7706bcf62a7f3e69175be1c3d35b
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-29 09:29:33 -0500

    added functionality for storing multiple items in cart, mongo Db cloud atlas is showing the correct quantities after add to cart is pressed

--- 12-NoSQL-MongoDB/models/user.js
+++ 12-NoSQL-MongoDB/models/user.js
@@ -44,16 +44,40 @@ addToCart(product){
     // Now we can assume we have a cart 
     // property on our User, and we can 
     // check if the cart contains a product already
-    // const cartProduct = this.cart.items.findIndex((cp) => {
-    //     return cp._id === product._id;
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+        return cp.productId.toString() === product._id.toString();
 
-    // });
+    });
+
+    let newQuantity = 1;
+        // const updatedCart = {items: [{...product, quantity: 1}]};
+        const updatedCartItems = [...this.cart.items];
+        // Above we create a new array where we spread 
+        // in all the elements
+        // And we can now edit the above updatedCartItems 
+        // array W/O touching the old array due to the nature 
+        // of JS reference primitive types
+    
+    if(cartProductIndex >= 0){
+      // if true, it means this product already 
+      // exists 
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+      // So above if we make it here we know that it exists, 
+      // so we can set it's quantity to the new quantity
+
+    } else{
+        // else if the item does not exist yet
+        updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity})
+    }
     // product.quantity = 1;
     // the above is how we add properties on 
     // the fly in JavaScript, we can also 
     // add it below:
-    // const updatedCart = {items: [{...product, quantity: 1}]};
-    const updatedCart = {items: [{productId: new ObjectId(product._id), quantity: 1}]};
+
+    const updatedCart = {
+     items: updatedCartItems
+    };
     const db = getDb();
     return db.collection('users').updateOne({_id: new ObjectId(this._id)},
     // The below will not merge the old cart with the 
