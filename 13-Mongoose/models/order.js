4bf06142d9f11ccf122a4bef6192ec886da1f240
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-05 08:01:19 -0500

    Added orderSchema for orders model, as well as exports for it

--- 13-Mongoose/models/order.js
+++ 13-Mongoose/models/order.js
@@ -11,5 +11,18 @@ const orderSchema = new Schema ({
         product: { type: Object,required: true},
         quantity: { type: Number, required: true }
         }
-    ]
+    ],
+    user: {
+        name: {
+            type: String,
+            required: true
+        },
+        userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+        }
+    }
     })
+
+    module.exports = mongoose.model('Order', orderSchema)
