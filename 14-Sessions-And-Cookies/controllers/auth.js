6f5ecf397eb6bf9d5f8e082d24cb24fd7d08d47c
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-10 08:32:17 -0500

    Added a cookie! after navigating to the products page, and looking in the network tab, we see that our cookie went along for the ride!

--- 14-Sessions-And-Cookies/controllers/auth.js
+++ 14-Sessions-And-Cookies/controllers/auth.js
@@ -7,7 +7,24 @@ exports.getLogin = (req, res, next) => {
   };
   
   
+
   exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/')
-};
+    
+    // We set the header in our response,
+    // and we pass in 'Set-Cookie'
+    // this 'Set-Cookie' is reserved, it's 
+    // a special phrase which... sets a cookie!
+
+    res.setHeader('Set-Cookie', 'loggedIn=true' );
+
+    // and the second arg is the value of the header, 
+    // which is a key value pair
+
+    res.redirect('/');
+}
+// And yes, if we look at the dev tools, 
+// and in the application tab, scroll down 
+// to cookies, we see our cookie!
+
+// Name: loggedIn
+// Value: true
