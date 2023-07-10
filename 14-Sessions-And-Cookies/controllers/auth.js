1fe2290b8cd0db71ea43d9ca2d092e93bdfb6bf3
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-10 08:50:36 -0500

    Extracted the true value from the set cookie, and passed it into isAuthenticated, after reloading, we see 'Add Product' and 'Admin Products' are now back and available in the navigation

--- 14-Sessions-And-Cookies/controllers/auth.js
+++ 14-Sessions-And-Cookies/controllers/auth.js
@@ -1,15 +1,43 @@
 exports.getLogin = (req, res, next) => {
+  // console.log(req.get('Cookie'));
+  // Now that below we added our cookie, let's 
+  // log it to the console and see what we get, 
+  // remember, in the network tab it's set under
+  // 'Cookie' so that's the req.get('Cookie') value
+  // which we see is loggedIn=true
+
+  // When we actually extract the value below, 
+  // we'll utilize some string methods to 
+  // get the value we need so instead of 
+
+  // const isLoggedIn = req.get('Cookie')
+
+  // We'll instead trim and split so we just get
+  // our boolean value true:
+
+  const isLoggedIn = req 
+        .get('Cookie')
+        .split(';')[2]
+        .trim()
+        .split('=')[1];
+console.log(isLoggedIn) // true
        res.render('auth/login', {
           path: '/login',
           pageTitle: 'Login',
-          isAuthenticated: req.isLoggedIn
+          // isAuthenticated: req.isLoggedIn
+          // now we can just pass in the value 
+          // below since we trimmed it
+          isAuthenticated: isLoggedIn
+          // Now if we reload the page we see 
+          // our two options in the navbar are back!
+          // we now have add product and Admin products
         });
   };
   
   
 
   exports.postLogin = (req, res, next) => {
-    
+
     // We set the header in our response,
     // and we pass in 'Set-Cookie'
     // this 'Set-Cookie' is reserved, it's 
@@ -28,3 +56,13 @@ exports.getLogin = (req, res, next) => {
 
 // Name: loggedIn
 // Value: true
+
+// And now that our cookie is set, the browser 
+// sends it to the server for every request that's 
+// now made 
+
+// and now let's click on the products link, 
+// which takes us to the product page, then 
+// look at the network tab, we see the 200 ok 
+// GET request, and if we scroll down to request 
+// headers, we see our cookie!! loggedIn=true
