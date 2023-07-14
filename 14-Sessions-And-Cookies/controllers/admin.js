61361974b2892d63e174b96de1a4248a586ef6ec
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-14 08:24:15 -0500

    everything is working again with the checking if user is logged in, and the add to cart from the details page works

--- 14-Sessions-And-Cookies/controllers/admin.js
+++ 14-Sessions-And-Cookies/controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn
   });
 };
 
