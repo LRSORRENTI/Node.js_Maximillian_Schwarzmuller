8c3e7abbc692fa1d311786a994e1b870931ad4e5
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-15 08:02:33 -0500

    added logic to postSignup method, where we have a check first to see if the user email already exists in mongoDB, if not then we can go ahead and create a new user and redirect to /login after input

--- 15-Authentication/controllers/auth.js
+++ 15-Authentication/controllers/auth.js
@@ -29,7 +29,47 @@ exports.postLogin = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  // the above are the three values that should reach 
+  // this route
+// We also need to check if an email already 
+// exists: 
+User.findOne({email: email})
+// above we're checking if the left side email field, 
+// matched what's inside of const email = req.body.email,
+// basically does the key match the passed in email, 
+// left side is database, right side user input 
+.then(userDoc => {
+  // then we promise chain to check if user exists,
+  // if userDoc is true, that means we have a user
+  // with that email already, so the client user 
+  // needs to use a different email
+  // so if UserDoc return a redirect back to signup
+  if(userDoc){
+    // should also display a message to the user, 
+    // right now we only have it redirecting
+    res.redirect('/signup')
+  }
+  // Now if we make it here, we've confirmed that 
+  // the email input is valid, it doesn't yet exist
+  // in our mongoDB atlas, so we can create a new one
+  const user = new User({
+    email: email,
+    password: password,
+    cart: { items: [] }
+  })
+  // and after updating user model, we can call
+  // .save() to save this user
+  return user.save();
+}).then(result => {
+  // we'll also chain another promise, where we redirect
+  res.redirect('/login')
+})
+.catch(err => console.log(err))
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
