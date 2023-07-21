aee2903007fc59f6a45e4ec6666d738699730fd5
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-21 09:10:30 -0500

    Added more validator checks for password, setting min and max length, chaining on methods for checks

--- 18-Validation/routes/auth.js
+++ 18-Validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -22,7 +22,28 @@ router.post('/login', authController.postLogin);
 // router.post('/signup', check('email').isEmail().withMessage().isAlphanumeric(), authController.postSignup);
 // we can chain on as many checks as we want like isAlphanumeric to check 
 // that all values are Aa-Zz and 0-9
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post('/signup', [
+ check('email').
+isEmail()
+.withMessage('Please enter a valid email')
+.custom((value, {req}) => {
+    if(value === 'forbidden@forbidden.com'){
+        throw new Error('This email is forbidden');
+    }
+    return true;
+}),
+body('password', 'Please enter a password with numbers, text, at least 5')
+// now we can also omit the withMessage method, and just 
+// pass in the above string as a second argument for a default 
+// message for any password check fail, otherwise we'd need 
+//  to chain withMessage for every check
+.isLength({min: 5, max: 40})
+// In production you'd want to specify a much more secure 
+// password, increasing the min amount,
+// and of course omitting .isAlphanumeric so !@#$ 
+// special characters are also valid for better security
+.isAlphanumeric()
+], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
