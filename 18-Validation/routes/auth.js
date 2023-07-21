40d15c869c094663961911d90aa3e798a868022a
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-21 08:37:37 -0500

    Added additional validator and chained on the .withMessage('Please enter a valid email') to the post route, now when an invalid email is entered instead of object: Object or Invalid value, we see that full please enter string

--- 18-Validation/routes/auth.js
+++ 18-Validation/routes/auth.js
@@ -15,7 +15,14 @@ router.post('/login', authController.postLogin);
 // remember we can add as many middlewares as we want, and we 
 // pass in 'email' since that's the name="email" from our
 // signup.ejs template
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+// router.post('/signup', check('email').isEmail(), authController.postSignup);
+
+// We can also chain on the withMessage method
+
+// router.post('/signup', check('email').isEmail().withMessage().isAlphanumeric(), authController.postSignup);
+// we can chain on as many checks as we want like isAlphanumeric to check 
+// that all values are Aa-Zz and 0-9
+router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
