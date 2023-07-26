85becb29a9839e12126a87924ed3d1e0414e6585
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-26 09:49:41 -0500

    removed dummy sendmail api key, isn't connected, i've gitignored actual api key

--- 20-File-Upload-Download/controllers/auth.js
+++ 20-File-Upload-Download/controllers/auth.js
@@ -10,8 +10,6 @@ const User = require('../models/user');
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key:
-        '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'
     }
   })
 );
