00140040f16274aad1113e39656cbc9b9f5e6abf
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-31 08:46:54 -0500

    debugging why isLoggedIn is not returning true, I tried hard passing true to isLoggedIn but csrf starts throwing errors, I'll keep trying

--- 21-Pagination/app.js
+++ 21-Pagination/app.js
@@ -110,11 +110,13 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500');
+  // res.redirect('/500')
+  
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+    // isAuthenticated: req.session.isLoggedIn
+     isAuthenticated: true
   });
 });
 
