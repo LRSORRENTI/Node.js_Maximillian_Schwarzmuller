a514c19265fd790bf31abc0c7a901213b41ba2dd
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-31 08:54:25 -0500

    okay fixed the new error, now back to the old error where isLoggedIn is undefined, so undefined not null... no value and no check, null is check and no value

--- 21-Pagination/app.js
+++ 21-Pagination/app.js
@@ -115,8 +115,8 @@ app.use((error, req, res, next) => {
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
-    // isAuthenticated: req.session.isLoggedIn
-     isAuthenticated: true
+    isAuthenticated: req.session.isLoggedIn
+     
   });
 });
 
