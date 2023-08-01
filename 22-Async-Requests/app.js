992babd93ccc177e33ab32ea13b40f9bdf73280c
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-01 09:01:25 -0500

    Added section on ASYNC requests

--- 22-Async-Requests/app.js
+++ 22-Async-Requests/app.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config({path: './util/my.env'})
 
 const path = require('path');
@@ -10,7 +9,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
-const multer = require('multer')
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -19,33 +18,34 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPassword}@maxnode.mppqkhv.mongodb.net/shop`
+
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
-
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, 'images')
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
 
-const fileFilt = (req, file, cb) => {
-  if(file.mimetype === 'image/png' 
-  || file.mimetype === 'image/jpg'
-  || file.mimetype === 'image/jpeg'){
-  cb(null, true);
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
   } else {
-  cb(null, false);
+    cb(null, false);
   }
 };
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -55,14 +55,11 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(multer({dest: 'images', storage: fileStorage, fileFilter: fileFilt }).single('image'))
-
-
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/images', express.static(path.join(__dirname, 'iamges')));
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: 'my secret',
@@ -74,16 +71,13 @@ app.use(
 app.use(csrfProtection);
 app.use(flash());
 
-
 app.use((req, res, next) => {
-  console.log('line 79, app.js:',req.session)
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
 app.use((req, res, next) => {
-  console.log('line 86 appjs: ', req.session)
   // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
@@ -111,15 +105,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500')
-  
+  // res.redirect('/500');
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
     isAuthenticated: req.session.isLoggedIn
-     
   });
-  console.log('line 123 app.js', isAuthenticated)
 });
 
 mongoose
