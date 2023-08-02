4c249492171fb871a48264cc7ce1ceb5df81bced
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-02 09:20:48 -0500

    Added setup for connecting the routes to the controller

--- 23-REST-API/routes/feed.js
+++ 23-REST-API/routes/feed.js
@@ -9,7 +9,8 @@ const router = express.Router();
 // here is where we'll define some routes, first 
 // to /posts where later on we'll have some posts 
 
-
+// GET /feed/posts , any get requests will be handles 
+// by this feedController controller
 router.get('/posts', feedController.getPosts)
 // so the getposts method is the function that should 
 // execute for this route, and we also need to register 
