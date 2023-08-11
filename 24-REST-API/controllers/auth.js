a21ada882c143dfcb186f5ebbf985eb0b9bc8b0d
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-11 08:08:09 -0500

    Added bycrptjs package to auth controller, that way we're not storing passwords as plain text

--- 24-REST-API/controllers/auth.js
+++ 24-REST-API/controllers/auth.js
@@ -1,4 +1,6 @@
 const {validationResult} = require('express-validator/src/validation-result')
+const bcrypt = require('bcryptjs');
+
 
 const User = require('../models/user');
 // Inside our auth controller we need to bring in 
@@ -16,10 +18,28 @@ exports.signup = (req, res, next) => {
     }
     // if we make it past the above if check we know we have 
     // the data we need 
-    
+
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
+    bcrypt.hash(password, 12).
+    then(hashedPw => {
+        const user = new User({
+            email: email, 
+            password: hashedPw,
+            name: name
+        });
+        return user.save();
+    })
+    .then(result => {
+        res.status(201).json({message: "User Created!", userid: result._id})
+    })
+    .catch(err => {
+        if(!err.statusCode ){
+            err.statusCode = 500;
+        }
+        next(err)
+    }); 
 
 
 }
