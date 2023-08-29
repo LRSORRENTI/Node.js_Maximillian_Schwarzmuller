d38e35a5de02b500ea22118a49435ef2fa1bb20b
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-29 07:58:06 -0500

    Added else if check inside componentDidMount, where we check for that backend data.action = 'action'

--- 29-WebSockets/frontend/src/pages/Feed/Feed.js
+++ 29-WebSockets/frontend/src/pages/Feed/Feed.js
@@ -44,6 +44,8 @@ class Feed extends Component {
     socket.on('posts', (data) => {
       if(data.action === 'create'){
         this.addPost(data.post)
+      } else if(data.action === 'update'){
+        this.updatePost(data.post);
       }
     })
   }
@@ -202,18 +204,8 @@ updatePost = post => {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } 
-          // else if (prevState.posts.length < 2) {
-          //   updatedPosts = prevState.posts.concat(post);
-          // }
+         
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
