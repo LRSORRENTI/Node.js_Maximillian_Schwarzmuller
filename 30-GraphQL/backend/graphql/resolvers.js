a959ed7a1ed8d4e9496de20041ca1f3d8e5f8bcc
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-08-31 08:24:27 -0500

    Added the resolver setup inside of resolvers file

---
+++ 30-GraphQL/backend/graphql/resolvers.js
@@ -0,0 +1,8 @@
+// The resolver is an exported object where we 
+// need a hello method from our schema.js definition
+
+module.exports = {
+    hello() {
+        return 'Hello World'
+    }
+};
--- 30-GraphQL/backend/graphql/schema.js
+++ 30-GraphQL/backend/graphql/schema.js
@@ -4,7 +4,7 @@ const {buildSchema} = require('graphql');
 
 module.exports = buildSchema(`
     type RootQuery{
-        hello: String
+        hello: String!
     }    
 
 schema {
@@ -14,4 +14,9 @@ schema {
 
 // The above is a very basic query where we send 
 // a 'hello' query and expect to get back a string or 
-// some text  
+// some text, and that text is now defined in our 
+// resolvers.js file, it needs to be resolved!
+
+// We can also make the value for the return 
+// by appending a '!', this means it's required to 
+// be a string 
