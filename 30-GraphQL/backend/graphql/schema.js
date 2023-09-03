7c2bfc22ef823c95c481e0777379a9625b62b252
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-09-03 09:28:41 -0500

    added graphql schema design, exclamation points postfixed indicate required values

--- 30-GraphQL/backend/graphql/schema.js
+++ 30-GraphQL/backend/graphql/schema.js
@@ -1,20 +1,44 @@
 // Inside here we'll define our graphql schema, 
 // we first import the package from graphql:
+
+// Section 423: Defining a Mutation Schema 
 const {buildSchema} = require('graphql');
 
 module.exports = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
-    }
+
+type Post {
+    _id: ID!
+    title: String!
+    content: String! 
+    imageUrl: String!
+    creator: User!
+    createdAt: String! 
+    updatedAt: String!
+}
 
 
-    type RootQuery {
-        hello: TestData!
-    }    
+type User {
+    _id: ID! 
+    name: String!
+    email: String!
+    password: String!
+    status: String!
+    posts: [Post!]
+}
+
+
+input UserInputData {
+    email: String!
+    name: String!
+    password: String!
+}
+
+type RootMutation {
+   createUser(userInput: UserInputData): User!
+}
 
 schema {
-        query: RootQuery
+        mutation: RootMutation
     }
 `)
 
