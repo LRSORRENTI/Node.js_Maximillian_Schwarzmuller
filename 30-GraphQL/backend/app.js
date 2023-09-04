efb249916d0c6aa49b244d747fdd630a09bf5240
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-09-04 09:28:26 -0500

    Connected the front end signup handler, where we pass in a graphql query

--- 30-GraphQL/backend/app.js
+++ 30-GraphQL/backend/app.js
@@ -52,6 +52,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
 
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization' );
+   
+    if(req.method === "OPTIONS"){
+        return res.sendStatus(200)
+    }
+
     next()
 })
 
