dba1eaf74706e5a7ad9fb9e2384f8d6ad3887fea
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-09-04 08:03:52 -0500

    brought in validator package for adding input validation

--- 30-GraphQL/backend/graphql/resolvers.js
+++ 30-GraphQL/backend/graphql/resolvers.js
@@ -1,6 +1,7 @@
 // The resolver is an exported object where we 
 // need a hello method from our schema.js definition
 const bcrypt = require('bcryptjs')
+const Validator = require('validator')
 
 const User = require('../models/user')
 
