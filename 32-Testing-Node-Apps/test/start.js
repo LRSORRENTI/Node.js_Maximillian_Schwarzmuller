ac5a730af074e1ebac561f50824ce1f30f4f0157
Luke <lrsorrenti@hotmail.com>
2023-09-18 09:42:36 -0500

    added console output from running npm test

--- 32-Testing-Node-Apps/test/start.js
+++ 32-Testing-Node-Apps/test/start.js
@@ -12,5 +12,31 @@ it('Should Add Numbers Correctly', function(){
 it('Uppercase the string', function(){
     let name = 'luke'
     name = name.toUpperCase();
-    expect(name).to.equal('LUKE')
+    expect(name).to.equal('LUKe')
 });
+
+/* 
+npm test
+
+> nodejs-complete-guide@1.0.0 test
+> mocha
+
+
+
+  ✔ Should Add Numbers Correctly
+  1) Uppercase the string
+
+  1 passing (9ms)
+  1 failing
+
+  1) Uppercase the string:
+
+      AssertionError: expected 'LUKE' to equal 'LUKe'
+      + expected - actual
+
+      -LUKE
+      +LUKe
+      
+      at Context.<anonymous> (test/start.js:15:21)
+      at process.processImmediate (node:internal/timers:476:21)
+*/
