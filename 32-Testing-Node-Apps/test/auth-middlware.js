97a45963788c8097780b49c4713f5d3becf0b6a0
Luke <lrsorrenti@hotmail.com>
2023-09-20 09:21:15 -0500

    added unit test for one of the auth middleware functions

---
+++ 32-Testing-Node-Apps/test/auth-middlware.js
@@ -0,0 +1,27 @@
+const chai = require('chai')
+
+const expect = chai.expect;
+
+const authMiddleware = require('../middleware/is-auth.js');
+
+
+// We'll unit test this specific code from the is-auth.js
+// middleware file: 
+
+
+// module.exports = (req, res, next) => {
+//     const authHeader = req.get('Authorization');
+//     if (!authHeader) {
+//       const error = new Error('Not authenticated.');
+//       error.statusCode = 401;
+//       throw error;
+//     }
+
+it('should throw error if no auth middleware is present', function(){
+    const req = {
+        get: function() {
+            return null;
+        }
+    }
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.');
+});
--- 32-Testing-Node-Apps/test/oldStart.js
+++ 32-Testing-Node-Apps/test/oldStart.js
@@ -1,19 +1,19 @@
-const chai = require('chai')
+// const chai = require('chai')
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
-it('Should Add Numbers Correctly', function(){
-    const num1 = 2;
-    const num2 = 3;
-    expect(num1 + num2).to.equal(5);
-})
+// it('Should Add Numbers Correctly', function(){
+//     const num1 = 2;
+//     const num2 = 3;
+//     expect(num1 + num2).to.equal(5);
+// })
 
 
-it('Uppercase the string', function(){
-    let name = 'luke'
-    name = name.toUpperCase();
-    expect(name).to.equal('LUKe')
-});
+// it('Uppercase the string', function(){
+//     let name = 'luke'
+//     name = name.toUpperCase();
+//     expect(name).to.equal('LUKe')
+// });
 
 /* 
 npm test
