c489e8ec1410b1b173e040d28fb8aac03c130ab1
Luke <lrsorrenti@hotmail.com>
2023-10-04 09:26:05 -0500

    added todos typing, where we define a todo interface then use the interface for the array type todo

--- 35-Node-TS/app.ts
+++ 35-Node-TS/app.ts
@@ -1,5 +1,14 @@
 import express from 'express'
 
+import bodyParser, { BodyParser } from 'body-parser';
+
+import todosRoutes from './routes/todos'
+
+
 const app = express();
+
+app.use(bodyParser.json())
+
+app.use()
 
 app.listen(3000)
--- 35-Node-TS/routes/todos.ts
+++ 35-Node-TS/routes/todos.ts
@@ -1,3 +1,25 @@
 import { Router }from 'express';
 
+
+import { ToDo } from '../models/singleTodo'
+
+const todos: ToDo[] = [];
+
+
 const router = Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({todos: todos})
+})
+
+router.post('/todo', (req, res, next) => {
+     const newTodo: ToDo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+     }
+     todos.push(newTodo)
+})
+
+
+export default router;
