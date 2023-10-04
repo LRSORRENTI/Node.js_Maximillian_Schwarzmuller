10fc56f040bb9fbaf3fc152a4b67d60279a34ef0
Luke <lrsorrenti@hotmail.com>
2023-10-04 08:03:38 -0500

    finished up generics example

--- 34-TypeScript-And-Node/javascript/05-Generics.js
+++ 34-TypeScript-And-Node/javascript/05-Generics.js
@@ -1,21 +1,16 @@
+"use strict";
 // TS Generics 
-
 // Generics are types that are able to interact 
 // with another type like: 
-
-const dataArr: Array<boolean> = [];
-
+const dataArr = [];
 // Above we have an array type, and wrapped inside 
 // boolean type, now we have a generic
-
 // Another generic example are promises 
-
-const myPromise = new Promise<string>((resolve, reject) => {
+const myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('Resolves after 3s')
-    }, 3000)
-})
-
-myPromise.then( result =>{
-    console.log(result.split(' '))
-}).catch(err => console.log)
+        resolve('Resolves after 3s');
+    }, 3000);
+});
+myPromise.then(result => {
+    console.log(result);
+}).catch(err => console.log(err));
