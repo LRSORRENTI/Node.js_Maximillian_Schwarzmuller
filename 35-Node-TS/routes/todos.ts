8434d5a362cc89aa84c60dd337a84d200c5b3449
Luke <lrsorrenti@hotmail.com>
2023-10-04 13:54:53 -0500

    Added some type casting in todos.ts route, adding custom typing

--- 35-Node-TS/routes/todos.ts
+++ 35-Node-TS/routes/todos.ts
@@ -3,10 +3,14 @@ import { Router }from 'express';
 
 import { Todo } from '../models/singleTodo'
 
+type RequestBody = { text: string };
+type RequestParams = {todoId: string };
+
 let todos: Todo[] = [];
 
 
 const router = Router();
+
 
 
 router.get('/', (req, res, next) => {
@@ -14,9 +18,10 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/todo', (req, res, next) => {
+     const body = req.body as RequestBody;
      const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
      }
      todos.push(newTodo)
      res.status(201).json({
@@ -27,19 +32,21 @@ router.post('/todo', (req, res, next) => {
 
 router.put('/todo/:todoId', (req, res, next) => {
     const tid = req.params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex(todoItem => {
         todoItem.id === tid;
     });
     if(todoIndex >= 0){
         // If the index is more than 0, we know we've got at least one
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text};
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text};
         return res.status(200).json({message: 'updated Todo', todos: todos })
     }
     res.status(404).json({message: 'Could not find todo with that id'})
 })
 
 router.delete('/todo/:todoId', (req, res, next) => {
-     todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+    const params = req.params as RequestParams;
+     todos = todos.filter(todoItem => todoItem.id !== params.todoId)
      // if the todo item id doesn't equal the req params todoid,
      res.status(200).json({message: 'Deleted Todo', todos: todos });
 })
