ebbbfa55317a3db3887fda66f805376767e2f533
Luke <lrsorrenti@hotmail.com>
2023-10-04 09:35:02 -0500

    Added put route for todos.ts

--- 35-Node-TS/routes/todos.ts
+++ 35-Node-TS/routes/todos.ts
@@ -19,7 +19,23 @@ router.post('/todo', (req, res, next) => {
         text: req.body.text
      }
      todos.push(newTodo)
+     res.status(201).json({
+        message: 'Added todo successfully',
+        todo: newTodo,
+        todos: todos })
 })
 
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => {
+        todoItem.id === tid;
+    });
+    if(todoIndex >= 0){
+        // If the index is more than 0, we know we've got at least one
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text};
+        return res.status(200).json({message: 'updated Todo', todos: todos })
+    }
+    res.status(404).json({message: 'Could not find todo with that id'})
+})
 
 export default router;
